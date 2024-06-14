d43f7833ab065ec0391b6f8f96cbcdfece2761e7
zelda <108274766+Zel69@users.noreply.github.com>
2024-06-14 11:27:19 -0300

    2121

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPricipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-principal");
+const caixaResultado = document.querySelector(".caixa-principal");
+const caixaResultado = document.querySelector(".caixa-principal");
+
+const = perguntas [
+    enunciado: "Pergunta 1",
+    alterntiva: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+{
+    enunciado: "Pergunra 2",
+    alterntivas: [
+        "Alternativa 1"
+        "Alternativa 2"
+    ]
+}
+
